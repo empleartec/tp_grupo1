51e0549d2842bbb108477c35f5d61257576f83b1
Javier Mayer <Javier Mayer>
2016-02-10 15:21:05 -0300

    Pantalla principal integrada

--- app/src/main/java/com/example/javier/melomanofinal/PrincipalActivity.java
+++ app/src/main/java/com/example/javier/melomanofinal/PrincipalActivity.java
@@ -14,17 +14,9 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
         findViewById(R.id.BotonJugar).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
